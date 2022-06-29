710baea489aad7aacaf3bc6a464a3bac5fa654f3
haoshaofei <haoshaofei@kuaishou.com>
2022-06-29 09:35:43 +0800

    搭建spring源码环境

---
+++ springsource-study/src/main/java/com/hsf/spring/MainText.java
@@ -0,0 +1,20 @@
+package com.hsf.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hsf.spring.beans.Person;
+
+/**
+ * @author hsfxuebao
+ * Created on 2022-06-28
+ */
+public class MainText {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("beans.xml");
+		Person bean = context.getBean(Person.class);
+		System.out.println(bean);
+
+	}
+}
---
+++ springsource-study/src/main/java/com/hsf/spring/beans/Person.java
@@ -0,0 +1,17 @@
+package com.hsf.spring.beans;
+
+
+/**
+ * @author hsfxuebao
+ * Created on 2022-06-28
+ */
+
+public class Person {
+
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
